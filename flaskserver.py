4f1004472c81dc63ec5d5e96d862265579ca7018
Rebecca <rebecca.kane@outlook.com>
2017-11-02 14:26:01 +0000

    Running static index file

--- flaskserver.py
+++ flaskserver.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return app.send_static_file('index.html') # Load index.html file
+
+if __name__ == "__main__":
+    app.run()
