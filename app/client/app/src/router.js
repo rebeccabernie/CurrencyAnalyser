ea9e01ee62b73020fb9928a7e7904dfeed44c16e
taraokelly <taraokelly.dev@gmail.com>
2018-01-22 14:04:13 +0000

    Flask/Vue template

---
+++ app/client/app/src/router.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Navbar from './components/Navbar'
+import SubNavbar from './components/SubNavbar'
+
+import Dashboard from './views/PageOne'
+import PageTwo from './views/PageTwo'
+
+/*
+ Integrating the Vue Router into a Vue application, where we are already composing our application with components,
+ allows us to map the pre-existing components to routes.
+ */
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/dashboard' 
+    },
+    {
+      path: '/dashboard',
+      components: {
+        navbar: Navbar,
+        subnavbar: SubNavbar,
+        main: Dashboard
+      }
+    },
+    {
+      path: '/about',
+      components: {
+        navbar: Navbar,
+        subnavbar: SubNavbar,
+        main: PageTwo
+      }
+    },
+    /*
+     https://github.com/vuejs/vue-router/issues/866
+     Any invalid route, i.e. routes not specified above, will be redirected to the root path.
+    */
+
+    /*
+     TODO: create and redirect to error page.
+    */
+    {
+      path: '/*',
+      redirect: '/' 
+    }
+  ]
+})
