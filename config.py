b5703ae01078bf43fbf4a467105b0845022091ae
Tony OKelly <taraokelly.dev@gmail.com>
2018-04-11 14:37:45 +0100

    Optimized prep for heroku

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-REDIS_URL = "redis://localhost:6379" 
+import os
+
+REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379') #"redis://localhost:6379" 
 REDIS_CHAN_CURR = "currency"
 REDIS_CHAN_GRAPH = "currencygraph"
