6d3a3f5ddeda72f816c210e86c1ddedf43730cd8
Tony OKelly <taraokelly.dev@gmail.com>
2018-04-14 02:02:40 +0100

    Graph showing n amount of latest currencies

--- worker.py
+++ worker.py
@@ -1,32 +1,21 @@
-import redis, json, time
-from config import REDIS_URL, REDIS_CHAN_CURR, REDIS_CHAN_GRAPH
-
+import redis, json, time, datetime, schedule, os
+from config import REDIS_URL, REDIS_CHAN_GRAPH
+from dicthelper import DictHelper
 from forex_python.converter import CurrencyRates
 from forex_python.bitcoin import BtcConverter
-import datetime, schedule, os
 
 r = redis.from_url(REDIS_URL)
-
-# Adapted: https://stackoverflow.com/questions/29033259/how-to-iterate-over-dict-in-class-like-if-just-referring-to-dict
-class DictHelper(dict):
-        def __init__(self, *arg, **kw):
-            super(DictHelper, self).__init__(*arg, **kw)
-            self.choosebettername = super(DictHelper, self).keys()
-
-        def __iter__(self):
-            return iter(self.choosebettername)
-
-        def keys(self):
-            return self.choosebettername
-
-        def itervalues(self):
-            return (self[key] for key in self)
-
 tic = 30.0
 starttime = time.time()
+currencies = []
+chart_data = {
+        'labels': [],
+        'datasets': 
+        []
+        }
 
 # Generate a unique colour based on unique currency code.
-# Get the ASCII code values for the char's A-Y are 65-90
+# Get the ASCII code values for the char's A-Y are 65-90.
 def rgbChar(c):
     return str((((ord(c)-65)/25)*255))
 
@@ -36,36 +25,42 @@ while True:
     # Using forex to get latest data: https://media.readthedocs.org/pdf/forex-python/latest/forex-python.pdf
     c = CurrencyRates()
     b = BtcConverter()
-
-    chart_data = {
-        'labels': [],
-        'datasets': 
-        []
-        }
-    
     data = c.get_rates('EUR')
+    btc = b.get_latest_price('EUR')
     rates = DictHelper(data)
+    pop = False
 
     # Adapted from: https://stackoverflow.com/questions/30071886/how-to-get-current-time-in-python-and-break-up-into-year-month-day-hour-minu
     chart_data['labels'].append(time.strftime("%H:%M:%S"))
+    if len(chart_data['labels']) >= 20:
+        chart_data['labels'].pop(0)
+        pop = True
 
-    btc = b.get_latest_price('EUR')
-    chart_data['datasets'].append({
-        'label': 'BTC',
-        'backgroundColor': 'rgba('+rgbChar('Y')+','+rgbChar('T')+','+rgbChar('C')+', 0.65)',
-        'data': [btc]
-    })
-
-    for (key, value) in rates.items():
-        # print(key, value)
-        k = list(key)
+    if chart_data['datasets']:
+        i = 0
+        chart_data['datasets'][i]['data'].append(btc)
+        if pop:
+            chart_data['datasets'][i]['data'].pop(0)
+        for value in rates.values():
+            i = i + 1
+            chart_data['datasets'][i]['data'].append(value)
+            if pop:
+                chart_data['datasets'][i]['data'].pop(0)
+    else:
         chart_data['datasets'].append({
-            'label': key,
-            'backgroundColor': 'rgba('+rgbChar(k[0])+','+rgbChar(k[1])+','+rgbChar(k[2])+', 0.65)',
-            'data': [value]
+            'label': 'BTC',
+            'backgroundColor': 'rgba('+rgbChar('Y')+','+rgbChar('T')+','+rgbChar('C')+', 0.65)',
+            'data': [btc]
         })
+        for (key, value) in rates.items():
+            currencies.append(key)
+            k = list(key)
+            chart_data['datasets'].append({
+                'label': key,
+                'backgroundColor': 'rgba('+rgbChar(k[0])+','+rgbChar(k[1])+','+rgbChar(k[2])+', 0.65)',
+                'data': [value]
+            })
 
-    print(chart_data)
     chart = json.dumps(chart_data)
     r.set(REDIS_CHAN_GRAPH, chart)
     
