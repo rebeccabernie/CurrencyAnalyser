4666d741f4a0419d4f52021a01bf2a452f8e63fa
Tony OKelly <taraokelly.dev@gmail.com>
2018-04-15 11:58:06 +0100

    Hard coded list of currency codes and names

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379') #"redis://local
 REDIS_CHAN_CURR = "currency"
 REDIS_CHAN_GRAPH = "currencygraph"
 REDIS_CHAN_LIST = "currencylatest"
+
+CURR_CODES = ['BTC', 'AUD', 'CAD', 'CHF', 'CNY', 'GBP', 'HKD', 'IDR', 'JPY', 'KRW', 'USD', 'ZAR']
+CURR_NAMES = ['Bitcoin', 'Australian Dollar', 'Canadian Dollar', 'Swiss Franc', 'Chinese Yuan', 'British Pound', 'Hong Kong Dollar', 'Indian Rupee', 'Japanese Ken', 'South Korean Won', 'US Dollar', 'South African Rand']
